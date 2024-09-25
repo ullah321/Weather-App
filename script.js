42a17693d6963a4570b4e7127eeaaba6039ced23
Ahmad Ullah Ashraf <ahmadullahashraf2004@gmail.com>
2024-09-25 11:45:17 +0600

    Script file updated

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ async function checkWeather(city) {
     }
 
     document.querySelector(".weather").style.display = "block";
-    document.querySelector(".error").style.display = "block";
+    document.querySelector(".error").style.display = "none";
     }    
 };
 
